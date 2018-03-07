81048f3ef6ae21497c3ddaa669cd0fa3b438d368
wang <13269403044@163.com>
2018-03-07 18:37:55 +0800

    server-2

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 const http = require('http');
 const chalk = require('chalk');
-const conf = require('./config/defaultConf')
+const path = require('path');
+const fs = require('fs');
+const conf = require('./config/defaultConf');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<body>');
-  res.write('Hello World!');
-  res.write('</body>');
-  res.end('</html>');
-})
+  const filePath = path.join(conf.root, req.url);
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader('content-Type', 'text/plain');
+      res.end(`${filePath} is not a directory or file`);
+      return;
+    }
+
+    if (stats.isFile()) {
+      res.statusCode = 200;
+      res.setHeader('content-Type', 'plain');
+      res.createReadStream(filePath).pipe(res);
+    } else if (stats.isDirectory()) {
+      fs.readdir(filePath, (err, files) => {
+        res.statusCode = 200;
+        res.setHeader('content-Type', 'text/plain');
+        res.end(files.join(','));
+      })
+    }
+  })
+});
 
 server.listen(conf.port, conf.hostname, () => {
   const addr = `http://${conf.hostname}:${conf.port}`;
   console.info(`Server started at ${chalk.green(addr)}`);
-})
+});
