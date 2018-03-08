2434b3a0ce4c3c97bb53cc495e9d9807440ef02b
wang <13269403044@163.com>
2018-03-08 11:50:55 +0800

    template

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('./../config/defaultConf');
+
+const tplPath = path.join(__dirname, '../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString())
 
 module.exports = async function (req, res, filePath) {
   try {
@@ -14,13 +20,19 @@ module.exports = async function (req, res, filePath) {
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(files.join(','));
+      res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, filePath);
+      const data = {
+        files,
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : ''
+      }
+      res.end(template(data));
     }
   } catch (error) {
     res.statusCode = 404;
     res.setHeader('content-Type', 'text/plain');
-    res.end(`${filePath} is not a directory or file`);
+    res.end(`${filePath} is not a directory or file\n ${error.toString()}`);
     return;
   }
 }
